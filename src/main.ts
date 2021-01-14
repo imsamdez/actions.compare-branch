01986e7fbe59561d72dbf8226b40ba290c9ab127
sdubrez <samuel@1kubator.com>
2021-01-14 17:46:40 +0100

    feat(debug): add some debug messages

--- src/main.ts
+++ src/main.ts
@@ -8,26 +8,28 @@ const repo = github.context.repo;
 
 export async function run(): Promise<void> {
   try {
+    const baseName = core.getInput(ActionInputs.BRANCH_BASE);
+    const compareName = core.getInput(ActionInputs.BRANCH_COMPARE);
     const branches = await octokit.repos
       .listBranches({...repo})
       .then(response => response.data);
-    const base = branches.find(
-      (b) /** branch */ => b.name === core.getInput(ActionInputs.BRANCH_BASE)
-    );
-    const compare = branches.find(
-      (b) /** branch */ => b.name === core.getInput(ActionInputs.BRANCH_COMPARE)
-    );
+    const base = branches.find((b) /** branch */ => b.name === baseName);
+    const compare = branches.find((b) /** branch */ => b.name === compareName);
 
     if (base == null) {
       return core.setFailed(
-        `Base branch "${base}" do not exist on repository ${repo.owner}/${repo.repo}`
+        `Base branch "${baseName}" do not exist on repository ${repo.owner}/${repo.repo}`
       );
     }
     if (compare == null) {
       return core.setFailed(
-        `Compare branch "${compare}" do not exist on repository ${repo.owner}/${repo.repo}`
+        `Compare branch "${compareName}" do not exist on repository ${repo.owner}/${repo.repo}`
       );
     }
+    core.debug(
+      `${baseName} is at ${base.commit} and ${compareName} is at ${compare.commit}`
+    );
+    core.debug(`result is ${base.commit === compare.commit}`);
     return core.exportVariable(
       'COMPARE_RESULT_SAME',
       base.commit === compare.commit
